09e1867f7b5aaeb87d8c12d62fb9c4ff0a4bc5a3
Mance Rah <mance.development@gmail.com>
2020-10-09 17:49:22 -0400

    Add bootstrap classes to form fields

--- auctions/forms.py
+++ auctions/forms.py
@@ -14,3 +14,10 @@ class ListingsForm(forms.ModelForm):
             'initial_bid',
             'category'
         ]
+
+    def __init__(self, *args, **kwargs):
+        super(ListingsForm, self).__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+        })
